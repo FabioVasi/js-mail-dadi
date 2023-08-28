3439313f4ab3388b1b8efe384ad3d6e1677d5db2
FabioVasi <vasifabio96@gmail.com>
2023-08-28 16:13:37 +0200

    add mail error message

--- asset/js/script.js
+++ asset/js/script.js
@@ -16,8 +16,47 @@ Strumenti.
 
 */
 
+const userMails = ['pincopallo@outlook.it', 'mariorossi@gmail.com', 'ziotom@libero.it'];
 
+console.log(userMails);
 
+const listElement = document.querySelector('ul');
+
+for (let i = 0; i < userMails.length; i++) {
+    
+    const mail = userMails[i];
+
+    console.log(mail);
+
+    const listItem = `<li>${mail}</li>`
+   
+    document.getElementById('submit').addEventListener("click", function() {
+    
+        const userName = document.getElementById('mail').value;
+        
+        console.log(userName);
+    
+        if(userName === 'pincopallo@outlook.it') {
+    
+          console.log(mail);
+    
+        } else if(userName === 'mariorossi@gmail.com') {
+
+            console.log(mail);
+
+        } else if (userName === 'ziotom@libero.it') {
+
+            console.log(mail);
+
+        } else {
+
+            alert("Ops!, non puoi accedere a questa pagina, riprova.")
+
+        }
+    
+    })
+
+}
 
 
 
